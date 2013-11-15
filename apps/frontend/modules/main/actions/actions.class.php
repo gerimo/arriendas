6225ce7808fd044bc8430e0cf4bcc50a55b69cc2
fredskwara <fredskwara@gmail.com>
2013-11-15 17:31:18 -0300

    reporte danios sin extension

--- apps/frontend/modules/main/actions/actions.class.php
+++ apps/frontend/modules/main/actions/actions.class.php
@@ -315,15 +315,15 @@ public function executeGenerarReporte(sfWebRequest $request) {
 
     //Creamos la instancia de la libreria mPDF
     require sfConfig::get('sf_app_lib_dir')."/mpdf53/mpdf.php";
- //   $this->getResponse()->setContentType('application/pdf');
+   $this->getResponse()->setContentType('application/pdf');
     $pdf= new mPDF();
 
     //Generamos el HTML correspondiente
     $request->setParameter("idAuto",$request->getParameter("idAuto"));
     $html=$this->getController()->getPresentationFor("main","informeDanios");
-   // $pdf->WriteHTML($html,0);
-  //  return $this->renderText($pdf->Output());
-    return $this->renderText($html);
+    $pdf->WriteHTML($html,0);
+    return $this->renderText($pdf->Output());
+  //  return $this->renderText($html);
 }
 
 public function executeGenerarReporteResumen(sfWebRequest $request){
