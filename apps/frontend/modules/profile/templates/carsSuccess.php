5055ec08bce0a6fd31b3ed2bd12f3d14fa7a3c3b
Juan Manuel Aguero <juanma.aguero@gmail.com>
2014-06-02 23:36:21 -0300

    Fix de versiones de archivos estaticos

--- apps/frontend/modules/profile/templates/carsSuccess.php
+++ apps/frontend/modules/profile/templates/carsSuccess.php
@@ -1,7 +1,7 @@
 
 <?php use_stylesheet('calendario.css') ?>
 <?php use_stylesheet('comunes.css') ?>
-<?php use_stylesheet('mis_autos.css') ?>
+<?php use_stylesheet('mis_autos.css??v=02052014') ?>
 <?php use_stylesheet('registro.css') ?>
 <?php
 $useragent=$_SERVER['HTTP_USER_AGENT'];
--- apps/frontend/modules/profile/templates/oportunidadesSuccess.php
+++ apps/frontend/modules/profile/templates/oportunidadesSuccess.php
@@ -1,8 +1,8 @@
 <?php use_stylesheet('calendario.css') ?>
 <?php use_stylesheet('registro.css') ?>
 <?php use_stylesheet('mis_arriendos.css') ?>
-<?php use_stylesheet('comunes.css') ?>
-<?php use_javascript('pedidos.js?v=28012013') ?>
+<?php use_stylesheet('comunes.css?v=02052014') ?>
+<?php use_javascript('pedidos.js?v=02052014') ?>
 <?php use_stylesheet('cupertino/jquery-ui.css') ?>
 <?php
 $useragent=$_SERVER['HTTP_USER_AGENT'];
