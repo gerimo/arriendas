4813a108513bb3a022fc9b3c3e1e63586a64106b
Juan Manuel Aguero <juanma.aguero@gmail.com>
2014-06-30 20:08:15 -0300

    Se implementa proceso symfony para actualizacion de campos de tabla Car

---
+++ lib/task/AddState2CarTask.class.php
@@ -0,0 +1,77 @@
+<?php
+
+class AddState2CarTask extends sfBaseTask {
+
+    protected function configure() {
+        // // add your own arguments here
+        // $this->addArguments(array(
+        //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+        // ));
+
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+                // add your own options here
+        ));
+
+        $this->namespace = 'arriendas';
+        $this->name = 'AddState2Car';
+        $this->briefDescription = '';
+        $this->detailedDescription = <<<EOF
+The [AddProvince2Car|INFO] task does things.
+Call it with:
+
+  [php symfony AddState2Car|INFO]
+EOF;
+    }
+
+    protected function execute($arguments = array(), $options = array()) {
+
+        $config = ProjectConfiguration::getApplicationConfiguration("frontend", "prod", TRUE);
+        sfContext::createInstance($config);
+
+        echo "verificando...\n";
+        $cars = Doctrine_Core::getTable('car')->findAll();
+        $processed = 0;
+        $updated = 0;
+        //echo "searching cars...\n";
+        foreach ($cars as $car) {
+            //echo "processing car id:" . $car->getId();
+            /* try comuna */
+            $comunaId = $car->getComunaId();
+            if ($comunaId) {
+                $comuna = Doctrine_Core::getTable('comunas')->findOneByCodigoInterno($comunaId);
+            }
+            if (!$comuna) {
+                /* try comuna by city */
+                $comunaId = $car->getCity()->getCodigoComuna();
+                if ($comunaId) {
+                    $comuna = Doctrine_Core::getTable('comunas')->findOneByCodigoInterno($comunaId);
+                }
+            }
+
+            /* only with valid comuna */
+            if ($comuna) {
+                $stateId = $comuna->getPadre();
+                $oldStateId = $car->getStateId();
+                //echo "    oldStateId:" . $oldStateId . " | stateId:" . $stateId;
+                if ($oldStateId != $stateId) {
+                    $car->setStateId($stateId);
+                    $car->save();
+
+                    /* updated count */
+                    $updated++;
+                }
+            }
+            echo ". ";
+            /* processed count */
+            $processed++;
+        }
+        echo " \n";
+        $this->log('processed:' . $processed);
+        $this->log('updated:' . $updated);
+        $this->log('done.');
+    }
+
+}
