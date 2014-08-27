30ba32d71ba0267ab9f1574224606316554706fa
Juan Manuel Aguero <juanma.aguero@gmail.com>
2014-08-27 00:14:16 -0300

    Chequeo de coincidencia de apellido de usuario

--- lib/model/ScraperService.class.php
+++ lib/model/ScraperService.class.php
@@ -25,42 +25,59 @@ class ScraperService {
         $rundv = $portions[1];
         $client = new \Goutte\Client();
 
-        $this->_log("check bloqueado", "info", "se llamo al servicio");
+        $this->_log("getLicenceStatus", "info", "se llamo al servicio");
         $crawler = $client->request('POST', 'http://www.srcei.cl/bloqueo/mostrarLicencia.jsp', array('accion' => "C", 'RUN' => $rut, 'RUNNUM' => $run, 'RUNDV' => $rundv));
-        
+
+        $nodeCount = count($crawler->filter('td.textodatos'));
+        $chequedNodeCount = 0;
+
+        /* conditions */
+        $lastname_match = false;
+        $blocked_date_found = false;
+        $problems_found = false;
+
         foreach ($crawler->filter('td.textodatos') as $node) {
             /* si encuentra al nombre esta bloqueada */
             if (preg_match("/" . $lastname . "/i", $node->nodeValue)) {
-                $this->_log("check bloqueado", "info", "encontro el nombre");
-                $status = 2;
+                $this->_log("getLicenceStatus", "info", "encontro el nombre");
+                $lastname_match = true;
             }
-            
-            /* bloqueado */
-            if (preg_match("/Fecha Bloqueo/i", $node->nodeValue)) {
-                $this->_log("check bloqueado", "info", "encontro fecha de bloqueo");
-                $status = 2;
-            }
-            
+
             /* problemas para procesar */
             if (preg_match("/Problemas para procesar requerimiento/i", $node->nodeValue)) {
-                $this->_log("check bloqueado", "info", "encontro problemas");
-                $status = 0;
+                $this->_log("getLicenceStatus", "info", "encontro problemas");
+                $problems_found = true;
             }
+            $chequedNodeCount++;
         }
+
         foreach ($crawler->filter('td.textocuerpo') as $node) {
             /* bloqueado */
             if (preg_match("/Fecha Bloqueo/i", $node->nodeValue)) {
-                $this->_log("check bloqueado", "info", "encontro fecha de bloqueo");
-                $status = 2;
+                $this->_log("getLicenceStatus", "info", "encontro fecha de bloqueo");
+                $blocked_date_found = true;
             }
         }
+
+        if ($blocked_date_found) {
+            $status = 2;
+        }
+        if (!$lastname_match && $chequedNodeCount >= $nodeCount) {
+            $status = 2;
+        }
+        if ($problems_found) {
+            $status = 0;
+        }
+
         return $status;
     }
 
     protected function _log($step, $status, $msg) {
-        $logPath = sfConfig::get('sf_log_dir') . '/scraper.log';
-        $custom_logger = new sfFileLogger(new sfEventDispatcher(), array('file' => $logPath));
-        $custom_logger->info($step . " - " . $status . ". " . $msg);
+        if (sfConfig::get('sf_environment') == "dev") {
+            $logPath = sfConfig::get('sf_log_dir') . '/scraper.log';
+            $custom_logger = new sfFileLogger(new sfEventDispatcher(), array('file' => $logPath));
+            $custom_logger->info($step . " - " . $status . ". " . $msg);
+        }
     }
 
 }
