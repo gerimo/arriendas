f13a23f8b4c33abdc7c2432db0d8bf28c5c166b8
Marco <marco@hakoom.cl>
2014-11-10 15:23:15 -0300

    test mail

--- lib/task/OportunityEmailQueueTask.class.php
+++ lib/task/OportunityEmailQueueTask.class.php
@@ -5,7 +5,7 @@ class OportunityEmailQueueTask extends sfBaseTask {
     protected function configure() {
 
         $this->addOptions(array(
-            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'frontend'),
             new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
             new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
             new sfCommandOption('numberOfMails', null, sfCommandOption::PARAMETER_REQUIRED, 'Cantidad de correos', 16),
@@ -25,7 +25,7 @@ EOF;
     protected function execute($arguments = array(), $options = array()) {
 
         $config = ProjectConfiguration::getApplicationConfiguration("frontend", "prod", TRUE);
-        sfContext::createInstance($config);
+        $context = sfContext::createInstance($this->configuration);
 
         // Este task debiera correr cada 1 minuto app
 
@@ -34,7 +34,11 @@ EOF;
         $conn = $databaseManager->getDatabase($options['connection'])->getConnection();
 
         // Este task toma un registro por cada reserva y envia un correo de oportunidad al dueño
+        $host       = 'http://www.arriendas.cl';
+        $routing    = $this->getRouting();
+        $imageUrl   = $host . $routing->generate('checkOpenedEmail');
 
+        // Se obtiene una oportunidad por cada reserva diferente
         $table = Doctrine_Core::getTable('OportunityEmailQueue');
         $q = $table
             ->createQuery('o')
@@ -50,20 +54,20 @@ EOF;
 
             $this->log($renter);
 
-            $messageBody = "<p>Usuario: " . "yo" . ":</p>";
-            $messageBody = "<img src=''>";
-            $messageBody .= "<p>mail: " . "mi@email.com" . ":</p>";
-            $messageBody .= "<p>licencia: " . "14083627-3" . ":</p>";
-            $messageBody .= "<p>No se pudo chequear la licencia por problemas de conexion con la web.</p>";
+            $body = '<p>Correo de prueba</p>';
+            $body .= '<img src="{$imageUrl}?id={$oportunityEmail->getId()}">';
+
+
             $message = $this->getMailer()->compose();
             $message->setSubject("No se pudo chequear la licencia");
             $message->setFrom('notificaciones@arriendas.cl', 'Notificaciones Arriendas');
             $message->setTo('marco.pincheira.s@gmail.com');
-            $message->setBody($messageBody, "text/html");
-            //$this->getMailer()->send($message);
+            $message->setBody($body, "text/html");
 
-            echo $messageBody;
+            $this->getMailer()->send($message);
+
+            $oportunityEmail->setSendedAt(date('Y-m-d H:i:s'));
+            $oportunityEmail->save();
         }
-
     }
 }
