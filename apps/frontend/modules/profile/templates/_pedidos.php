b875ea46a6b45341b1d72cd0c95aad6ad9d96d5f
Cristóbal Medina Moenne <cristobal@hakoom.cl>
2014-11-11 20:49:05 -0300

    cambios

--- apps/frontend/modules/profile/templates/_pedidos.php
+++ apps/frontend/modules/profile/templates/_pedidos.php
@@ -242,7 +242,7 @@ if (!$mostrarReservasRealizadas && !$mostrarReservasRecibidas) {
                 echo "<a href='#' id='contrato_" . $reserva['idReserve'] . "_" . $reserva['carId'] . "_" . $reserva['token'] . "' class='descargarContrato'>Descargar Contratos</a>";
                 echo "</div>";
                 
-                if ($reserva['completed'] == 0) {
+                if ($reserva['completed']) {
                     echo "<div class='pago'>";
                     echo "  <a href='#' id='extender_" . $reserva['idReserve'] . "' class='boton_extender " . $reserva['fechaInicio'] . "_" . $reserva['horaInicio'] . "_" . $reserva['fechaTermino'] . "_" . $reserva['horaTermino'] . "'>" . image_tag('img_pedidos/BotonExtender2.png', array("style" => "margin-top: 12px")) . "</a>";
                     echo "  <div style='text-align: center; margin-top: 3px;'>";
@@ -255,7 +255,7 @@ if (!$mostrarReservasRealizadas && !$mostrarReservasRecibidas) {
                     echo "  <div style='text-align: center; margin-top: 3px;'><a href=".url_for('messages/new?id='.$reserva['contraparteId'])." class='link-contactar' style='margin: auto' >Contactar</a></div>";
                     echo "</div>";
                 }
-                
+
                 echo "</div>";
             }
         }
