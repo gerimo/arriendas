7dfa73dcc06788da54b907249fb9dd25d18f67d3
Cristóbal Medina Moenne <cristobal@hakoom.cl>
2014-11-26 11:58:15 -0300

    fix

--- apps/frontend/modules/profile/actions/actions.class.php
+++ apps/frontend/modules/profile/actions/actions.class.php
@@ -3764,7 +3764,7 @@ error_log("RESERVA RECHAZAR: ".$reserve->getId());
                 $oportunityQueue = Doctrine_Core::getTable('OportunityQueue')->findOneByReserveId($reserva->getId());
 
                 //obtiene en que estado se encuentra (pagada(3), preaprobada(2), rechazada(1) y espera(0))
-                if ($transaction[0]['impulsive'] && $transaction[0]['completed'] == 0 && !$oportunityQueue && ($transaction[0]['transaccion_original'] == 0 || $transaction[0]['transaccion_original'] == null) && $horasFaltantes > 2) {
+                if ($transaction[0]['impulsive'] && $transaction[0]['completed'] == 0 && !$oportunityQueue && ($transaction[0]['transaccion_original'] == 0 || $transaction[0]['transaccion_original'] == null) /*&& $horasFaltantes > 2*/) {
                     $estado = 7; // Se genera la reserva impulsive, pero no se paga
                 } else if ($reserva->getConfirmed() == 0 && $reserva->getImpulsive() && ($reserva->getReservaOriginal() == null || $reserva->getReservaOriginal() == 0)) {
                     $estado = 6; // Compra impulsiva, dueño original aún no confirma
