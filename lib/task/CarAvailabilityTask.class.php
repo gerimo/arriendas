1d10ea7711399b6d4d590348f18ad894ae984569
Cristóbal Medina Moenne <cristobal@hakoom.cl>
2014-12-05 13:03:11 -0300

    cambio de disponibilidad de auto a persona

--- lib/task/CarAvailabilityTask.class.php
+++ lib/task/CarAvailabilityTask.class.php
@@ -115,7 +115,7 @@ EOF;
                 ->createQuery('C')
                 ->where('C.activo = 1')
                 ->andWhere('C.seguro_ok = 4')
-                ->andWhere('C.user_id = 83');
+                ->andWhere('C.user_id = 924');
 
             $Cars = $q->execute();
 
