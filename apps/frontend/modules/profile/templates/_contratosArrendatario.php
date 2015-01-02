18c16b7ba434a4c547df23d0d55ec563a0bd310c
Sergio Quezada Rivera <sergio@arriendas.cl>
2015-01-02 11:45:43 -0300

    sergio

--- apps/frontend/modules/profile/templates/_contratosArrendatario.php
+++ apps/frontend/modules/profile/templates/_contratosArrendatario.php
@@ -1,8 +1,8 @@
 <div id='confirmarContratosArrendatario'>
-    <input type="checkbox" name="contratoArrendatario0" id="contratoArrendatario1" class='checkboxContrato'><p> En caso de siniestro, dejaré constancia en carabineros INMEDIATAMENTE.</p>
-    <input type="checkbox" name="contratoArrendatario1" id="contratoArrendatario1" class='checkboxContrato'><p> He visto el <a href='' class='informeDanios' target='_blank' style="color: blue">Informe de daños del veh&iacute;culo</a>.</p>
-    <input type="checkbox" name="contratoArrendatario2" id="contratoArrendatario2" class='checkboxContrato'><p> Entiendo ser responsable por daños producidos durante el arriendo por debajo del deducible de 5UF.</p>
-    <input type="checkbox" name="contratoArrendatario3" id="contratoArrendatario3" class='checkboxContrato'><p> Entiendo perder mi cobertura en caso de conducción en estado de ebriedad.</p>
-    <input type="checkbox" name="contratoArrendatario4" id="contratoArrendatario4" class='checkboxContrato'><p> <span id='textoBencinaArrendatario'>Si el arriendo es por menos de un día, pagaré por la bencina utilizada en efectivo según los kilómetros manejados. Si el arriendo es por un día o más, devolveré el veh&iacute;culo con el marcador de bencina en el mismo nivel con el que me fue entregado.</span></p>
-    <input type="checkbox" name="contratoArrendatario5" id="contratoArrendatario5" class='checkboxContrato'><p> Realizaré el pago desde una cuenta bancaria o medio de pago, a mi nombre.</p>
+    <p><input class="checkboxContrato" id="contratoArrendatario0" name="contratoArrendatario0" type="checkbox"> En caso de siniestro, dejaré constancia en carabineros INMEDIATAMENTE.</p>
+    <p><input class="checkboxContrato" id="contratoArrendatario1" name="contratoArrendatario1" type="checkbox"> He visto el <a href='#' target='_blank'>Informe de daños del vehículo</a>.</p>
+    <p><input class="checkboxContrato" id="contratoArrendatario2" name="contratoArrendatario2" type="checkbox"> Entiendo ser responsable por daños producidos durante el arriendo por debajo del deducible de 5UF.</p>
+    <p><input class="checkboxContrato" id="contratoArrendatario3" name="contratoArrendatario3" type="checkbox"> Entiendo perder mi cobertura en caso de conducción en estado de ebriedad.</p>
+    <p><input class="checkboxContrato" id="contratoArrendatario4" name="contratoArrendatario4" type="checkbox"> Si el arriendo es por menos de un día, pagaré por la bencina utilizada en efectivo según los kilómetros manejados. Si el arriendo es por un día o más, devolveré el vehículo con el marcador de bencina en el mismo nivel con el que me fue entregado.</p>
+    <p><input class="checkboxContrato" id="contratoArrendatario5" name="contratoArrendatario5" type="checkbox"> Realizaré el pago desde una cuenta bancaria o medio de pago, a mi nombre.</p>
 </div>
---
+++ apps/frontend/templates/newIndexLayout.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <?php //include_metas() ?>
+        <?php //include_title() ?>
+        <?php //include_stylesheets() ?>
+
+        <link href="http://arriendas.assets.s3.amazonaws.com/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+
+        <!-- JQuery UI -->
+        <link href="/css/newDesign/jquery-ui/jquery-ui.min.css" rel="stylesheet" type="text/css">
+        <link href="/css/newDesign/jquery-ui/jquery-ui.structure.min.css" rel="stylesheet" type="text/css">
+        <link href="/css/newDesign/jquery-ui/jquery-ui.theme.min.css" rel="stylesheet" type="text/css">
+        
+        <!-- Bootstrap -->
+        <link href="/css/newDesign/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+        <!-- Font Awesome -->
+        <link href="/css/newDesign/font-awesome/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+        <!-- Complementos -->
+        <link href="/css/newDesign/datetimepicker/jquery.datetimepicker.css" rel="stylesheet" type="text/css">
+        <link href="/css/newDesign/slick/slick.css" rel="stylesheet" type="text/css">
+        <link href="/css/newDesign/raty/jquery.raty.css" rel="stylesheet" type="text/css">
+
+        <!-- Estilos para todo el proyecto -->
+        <link href="/css/newDesign/arriendas.css" rel="stylesheet" type="text/css">
+
+        <!-- Estilos para específicos para el index -->
+        <link href="/css/newDesign/index.css" rel="stylesheet" type="text/css">
+
+        <!-- Fonts -->
+        <link href="http://fonts.googleapis.com/css?family=Roboto:400,300,700" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,700italic,800,400italic" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+
+        <!-- JQuery -->
+        <script src="/js/newDesign/jquery-2.1.3.min.js" type="text/javascript"></script>
+
+        <!-- JQuery UI -->
+        <script src="/js/newDesign/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+        
+        <!-- Bootstrap -->
+        <script src="/js/newDesign/bootstrap.min.js" type="text/javascript"></script>
+
+        <!-- Complementos -->
+        <script src="/js/newDesign/datetimepicker/jquery.datetimepicker.js" type="text/javascript"></script>
+        <script src="/js/newDesign/slick/slick.min.js" type="text/javascript"></script>
+        <script src="/js/newDesign/raty/jquery.raty.js" type="text/javascript"></script>
+        <script src="/js/newDesign/number/jquery.number.min.js" type="text/javascript"></script>
+
+        <?php include_partial("global/header") ?>
+
+        <?php echo $sf_content ?>
+
+        <?php include_partial("global/footer") ?>
+    </body>
+</html>
