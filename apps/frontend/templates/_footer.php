671305e3f5863abebc1c0df72bbdac9e5c740309
francoinostroza <franco.inostrozah@gmail.com>
2015-01-06 15:14:30 -0300

    video

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -47,7 +47,7 @@
 
     <div class="row">
         <div class="hidden-xs col-sm-12 col-md-12 text-center" id="address"> 
-            <p class= "direccion">Manuel Montt 404, Providencia, Santiago de Chile</p>
+            <p class= "direccion">Manuel Montt 1404, Providencia, Santiago de Chile</p>
             <p>
                 <i class="fa fa-phone"></i> <a href="tel:0223333714">(02) 2333-3714</a> 
                 <i class="fa fa-support"></i> <a href="mailto:soporte@arriendas.cl">soporte@arriendas.cl</a>
@@ -74,7 +74,7 @@
         <div class="col-xs-12 visible-xs hidden-sm visible-md">
             <h1><strong>Contacto</strong><h1>
             <p>Télefono (02) 2333-3714</p>
-            <p>Manuel Montt 404</p>
+            <p>Manuel Montt 1404</p>
             <p>Providencia, Santiago/Chile </p>
             <a class="fa fa-facebook fa-3x" href="#">facebook</a>
             <a class="fa fa-twitter fa-3x" href="#">twitter</a>
--- apps/frontend/templates/_header.php
+++ apps/frontend/templates/_header.php
@@ -52,7 +52,7 @@
                 </ul>
 
                 <ul class="nav navbar-nav navbar-right">
-                <li><a href="tel:+0223333714"><span class="glyphicon glyphicon-earphone"></span>(02) 2 333 37 14</a></li>
+                <li><a href="tel:+56223333714"><span class="glyphicon glyphicon-earphone"></span>(02) 2 333 37 14</a></li>
                 </ul>
 
             </div><!-- /.navbar-collapse -->
