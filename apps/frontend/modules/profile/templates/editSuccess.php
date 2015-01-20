86065e9c697e2ef3fe42557eb890d221444a2896
francoinostroza <franco.inostrozah@gmail.com>
2015-01-20 18:58:45 -0300

    license

--- apps/frontend/modules/profile/templates/editSuccess.php
+++ apps/frontend/modules/profile/templates/editSuccess.php
@@ -36,9 +36,9 @@
                 <div class="regis_foto_frame">
                     <div id="previewlicense">
                         <?php if ($User->getDriverLicenseFile() == null): ?>
-                            <?php echo image_tag('img_asegura_tu_auto/foto_padron_reverso.png', 'size=194x204') ?>
+                            <?php echo image_tag('pimg_asegura_tu_auto/foto_padron_reverso.png', 'size=194x204') ?>
                         <?php else: ?>
-                            <?php echo image_tag($User->getLicenceFileName(), 'size=194x204') ?>
+                            <?php echo image_tag('licence/'.$User->getLicenceFileName(), 'size=194x204') ?>
                         <?php endif ?>
                     </div> 
 
