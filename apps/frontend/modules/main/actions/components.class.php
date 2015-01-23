4a019244f3733bef44af393fac80518e645f2733
francoinostroza <franco.inostrozah@gmail.com>
2015-01-23 10:39:23 -0300

    fix

--- apps/frontend/modules/main/actions/components.class.php
+++ apps/frontend/modules/main/actions/components.class.php
@@ -5,26 +5,27 @@
         public function executeTerminosycondiciones() {}
 
         public function executeHeader(){
+
+            if ($this->gteUser()->isAuthenticated()) {
         	
-        	$userId = sfContext::getInstance()->getUser()->getAttribute('userid');
+            	$userId = sfContext::getInstance()->getUser()->getAttribute('userid');
 
-        	$this->hasCars = false;
-        	$this->hasActiveCars = false;
-        	
-        	$Cars = Doctrine_Core::getTable('Car')->findByUserId($userId);
-        	if (count($Cars)) {
+            	$this->hasCars = false;
+            	$this->hasActiveCars = false;
+            	
+            	$Cars = Doctrine_Core::getTable('Car')->findByUserId($userId);
+            	if (count($Cars)) {
 
-        		$this->hasCars = true;
+            		$this->hasCars = true;
 
-        		foreach ($Cars as $Car) {
-        			if ($Car->activo) {
-        				$this->hasActiveCars = true;
-        				break;
-        			}
-        		}
-        	}
-
-        	
+            		foreach ($Cars as $Car) {
+            			if ($Car->activo) {
+            				$this->hasActiveCars = true;
+            				break;
+            			}
+            		}
+            	}
+            }        	
         }
 
         public function executeFooter(){
