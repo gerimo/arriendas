4aebc2da46533034edb0a21977948950d98758f0
francoinostroza <franco.inostrozah@gmail.com>
2015-01-23 09:46:50 -0300

    components header

--- apps/frontend/modules/main/actions/components.class.php
+++ apps/frontend/modules/main/actions/components.class.php
@@ -3,5 +3,11 @@
     class mainComponents extends sfComponents {
         
         public function executeTerminosycondiciones() {}
+
+        public function executeHeader(){
+        	$carId= sfContext::getInstance()->getUser()->getAttribute('carId');
+        	error_log($carId);
+
+        }
     }
 ?>
