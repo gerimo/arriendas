d26e814bf1ceae438623255380d0b06b2aac10d1
Cristóbal Medina Moenne <cristobal@hakoom.cl>
2015-01-26 18:12:19 -0300

    fix

--- apps/frontend/modules/emails/templates/_opportunityMailing.php
+++ apps/frontend/modules/emails/templates/_opportunityMailing.php
@@ -4,20 +4,20 @@
 
 <p>Hola <?php echo $Owner->firstname ?>,</p>
 
-<p>Hemos recibido un pago por el arriendo de un auto con características muy similares a las de tu auto <?php echo strtoupper($Car->patente) ?>. Los datos de esta oportunidad son los siguientes: </p>
+<p>Hemos recibido un pago por el arriendo de un auto con características muy similares a las de tu auto patente <b><?php echo strtoupper($Car->patente) ?></b>. Los datos de esta oportunidad son los siguientes: </p>
 
 <table>
     <tr>
-        <th style='text-align: left'>Fecha de inicio</th>
-        <td><?php echo date("d-m-Y H:i", strtotime($Reserve->getFechaInicio2())) ?></td>
+        <td style='text-align: left'>Fecha de inicio</td>
+        <th style='text-align: left'><?php echo date("d-m-Y H:i", strtotime($Reserve->getFechaInicio2())) ?></th>
     </tr>
     <tr>
-        <th style='text-align: left'>Fecha de termino</th>
-        <td><?php echo date("d-m-Y H:i", strtotime($Reserve->getFechaTermino2())) ?></td>
+        <td style='text-align: left'>Fecha de termino</td>
+        <th style='text-align: left'><?php echo date("d-m-Y H:i", strtotime($Reserve->getFechaTermino2())) ?></th>
     </tr>
     <tr>
-        <th style='text-align: left'>Valor</th>
-        <td>$<?php echo number_format($Reserve->price, 0, ',', '.') ?></td>
+        <td style='text-align: left'>Valor</td>
+        <th style='text-align: left'>$<?php echo number_format($Reserve->price, 0, ',', '.') ?></th>
     </tr>
 </table>
 
