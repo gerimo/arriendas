f7b1a7eb05c4fd33bc8ce95cf1e8193ad9ab5de6
Cristóbal Medina Moenne <cristobal@hakoom.cl>
2015-02-06 17:09:17 -0300

    hotfix

--- apps/frontend/modules/emails/templates/_footer.php
+++ apps/frontend/modules/emails/templates/_footer.php
@@ -4,15 +4,7 @@
 <br><br>
 <?php if (isset($userId) && isset($mailingId)): ?>
 
-    <?php
-        error_log("HOST: ".$_SERVER ['HTTP_HOST']);
-        $host = "http://local.arriendas.cl";
-        if ($_SERVER ['HTTP_HOST'] == "www.arriendas.cl") {
-            $host = "http://www.arriendas.cl";
-        } elseif ($_SERVER ['HTTP_HOST'] == "dev.arriendas.cl") {
-            $host = "http://dev.arriendas.cl";
-        }
-    ?>
+    <?php $host = "http://www.arriendas.cl" ?>
 
     <?php $signature = md5("Arriendas.cl ~ ".$userId." ~ ".$mailingId) ?>
     <?php $url = str_replace("symfony", "", url_for('emails_unsubscribe', array("user_id" => $userId, "mailing_id" => $mailingId, "signature" => $signature))) ?>
