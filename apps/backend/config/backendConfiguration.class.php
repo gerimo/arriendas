6c607adbb5fb5a80153508cd9d966d7e79ca30a3
francoinostroza <franco.inostrozah@gmail.com>
2015-02-06 16:26:37 -0300

    url backend

--- apps/backend/config/backendConfiguration.class.php
+++ apps/backend/config/backendConfiguration.class.php
@@ -2,34 +2,33 @@
 
 class backendConfiguration extends sfApplicationConfiguration {
 
-    protected $frontendRouting = null;
+	protected $frontendRouting = null;
 
-    public function configure() {}
+    public function configure() {} 	
+ 
+  	public function generateFrontendUrl($name, $parameters = array()) {
 
-    public function generateFrontendUrl($name, $parameters = array()) {
-
-        $host = "local.arriendas.cl";
+        $host = "http://local.arriendas.cl";
         if ($_SERVER ['HTTP_HOST'] == "backend.arriendas.cl") {
-            $host = "arriendas.cl";
+            $host = "http://www.arriendas.cl";
         } elseif ($_SERVER ['HTTP_HOST'] == "dev.backend.arriendas.cl") {
-            $host = "dev.arriendas.cl";
+            $host = "http://dev.arriendas.cl";
         }
 
-        return $this->getFrontendRouting()->generate($name, $parameters);
-    }
+    	return $host.$this->getFrontendRouting()->generate($name, $parameters);
+  	}
 
-    public function getFrontendRouting() {
+ 	public function getFrontendRouting() {
 
-        if (!$this->frontendRouting) {
-
-          $this->frontendRouting = new sfPatternRouting(new sfEventDispatcher());
-     
-          $config = new sfRoutingConfigHandler();
-          $routes = $config->evaluate(array(sfConfig::get('sf_apps_dir').'/frontend/config/routing.yml'));
-     
-          $this->frontendRouting->setRoutes($routes);
-        }
-     
-        return $this->frontendRouting;
+	    if (!$this->frontendRouting) {
+	      $this->frontendRouting = new sfPatternRouting(new sfEventDispatcher());
+	 
+	      $config = new sfRoutingConfigHandler();
+	      $routes = $config->evaluate(array(sfConfig::get('sf_apps_dir').'/frontend/config/routing.yml'));
+	 
+	      $this->frontendRouting->setRoutes($routes);
+	    }
+	 
+	    return $this->frontendRouting;
     }
 }
