df0a209bab4d8876fc02c608b305eeb9a0c7f5f5
Cristóbal Medina Moenne <cristobal@hakoom.cl>
2015-02-09 13:43:56 -0300

    correcciones mailing

--- lib/task/CarAskAvailabilityTask.class.php
+++ lib/task/CarAskAvailabilityTask.class.php
@@ -42,16 +42,6 @@ EOF;
 
         $sentEmails = 0;
 
-        $week = array(
-            1 => "Lunes",
-            2 => "Martes",
-            3 => "Miércoles",
-            4 => "Jueves",
-            5 => "Viernes",
-            6 => "Sábado",
-            7 => "Domingo"
-        );
-
         try {
 
             $tomorrow = strtotime("+1 day");
