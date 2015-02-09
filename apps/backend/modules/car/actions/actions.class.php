1d68eea48eeda1e96d296529b6d5e364beebbfc7
Cristóbal Medina Moenne <cristobal@hakoom.cl>
2015-02-09 17:26:41 -0300

    se agrega limit al buscador de autos

--- apps/backend/modules/car/actions/actions.class.php
+++ apps/backend/modules/car/actions/actions.class.php
@@ -19,7 +19,7 @@ class carActions extends sfActions {
             $isMorePassengers   = $request->getPostParameter('isMorePassengers', false) === 'true' ? true : false;
 
 	        $Reserve    = Doctrine_Core::getTable('Reserve')->find($reserveId);
-	        $Cars       = Doctrine_Core::getTable('Car')->findCars($Reserve->getFechaInicio2(), $Reserve->getFechaTermino2(), 50, false, false, false, false, false, 13, false, $isAutomatic, $isLowConsumption, $isMorePassengers, false);
+	        $Cars       = Doctrine_Core::getTable('Car')->findCars($Reserve->getFechaInicio2(), $Reserve->getFechaTermino2(), 300, false, false, false, false, false, 13, false, $isAutomatic, $isLowConsumption, $isMorePassengers, false);
 
 	        $return["data"] = array();
 	        
@@ -38,8 +38,7 @@ class carActions extends sfActions {
 	                "user_fullname"        => $Car['user_name'],
 	                "user_telephone"       => $Car['user_telephone']
 	        	);
-	        }
-	        
+	        }	        
 
 	    } catch (Exception $e) {
 	        $return["error"] = true;
