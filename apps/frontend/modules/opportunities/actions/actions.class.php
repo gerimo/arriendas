c6e65537ff16ef364027ca593547e378e3146a81
Cristóbal Medina Moenne <cristobal@hakoom.cl>
2015-02-10 12:03:45 -0300

    fix

--- apps/frontend/modules/opportunities/actions/actions.class.php
+++ apps/frontend/modules/opportunities/actions/actions.class.php
@@ -172,7 +172,7 @@ class opportunitiesActions extends sfActions {
         }
 
         $O = $OriginalReserve->copy(true);
-        $O->setCarId($carId);
+        $O->setCar($Car);
         $O->setFechaReserva(date("Y-m-d H:i:s"));
         $O->setFechaConfirmacion(date("Y-m-d H:i:s"));
         $O->setConfirmed(true);
