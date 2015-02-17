915f586ad3c54322c8348e2d65f88fc277c83b02
francoinostroza <franco.inostrozah@gmail.com>
2015-02-17 10:41:55 -0300

    fix

--- apps/backend/modules/main/templates/_header.php
+++ apps/backend/modules/main/templates/_header.php
@@ -16,12 +16,12 @@
                 
                 <ul class="nav navbar-nav">
                     <li><a href="#">Reservas</a></li>
-                    <li class="dropdown">
+                   <!--  <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Autos<span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
                             <li><a href="<?php echo url_for('car_verify_car') ?>">Verificar auto</a></li>
                         </ul>
-                    </li>
+                    </li> -->
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Oportunidades<span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
