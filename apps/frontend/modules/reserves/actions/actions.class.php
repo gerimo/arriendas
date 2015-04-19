209d6d997963d491e290ed975a6f18668cc3ddb9
sergioquez <squezada2b08@gmail.com>
2015-04-19 08:52:38 -0300

    test

--- apps/frontend/modules/reserves/actions/actions.class.php
+++ apps/frontend/modules/reserves/actions/actions.class.php
@@ -490,7 +490,7 @@ class reservesActions extends sfActions {
 
         if ($request->hasParameter('warranty','payment-group','car','from','to')) {
             $warranty = $request->getPostParameter("warranty", null);
-            $payment  = $request->getPostParameter("payment", null); // Se saco la selección de khipu
+            $payment  = $request->getPostParameter("payment-group", null); // Se saco la selección de khipu
 
             $carId  = $request->getPostParameter("car", null);
             $from   = $request->getPostParameter("from", null);
@@ -653,13 +653,7 @@ class reservesActions extends sfActions {
             $this->redirect('reserve_airport');
         }
 
-        if ($payment == 1) {
-            $this->forward("khipu", "generatePayment");
-        } elseif ($payment == 2) {
-            $this->forward("webpay", "generatePayment");
-        } else {
-            $this->forward404();
-        }
+        $this->forward("khipu", "generatePayment");
     }
 
     public function executeReject (sfWebRequest $request) {
@@ -792,7 +786,7 @@ class reservesActions extends sfActions {
     }
 
     public function executeUploadLicenseWarning (sfWebRequest $request) {
-    
+        $error="";
         $return = array("error" => false);
         $valid_formats = array("jpg", "png", "gif", "bmp", "jpeg");
         try {            
@@ -858,13 +852,14 @@ class reservesActions extends sfActions {
                 }
                 $Reserve->save();
             }
+            $error = "problemas enviando el mail";
     
         } catch (Exception $e) {
             $return["error"] = true;
             if ($e->getCode() < 2) {
                 $return["errorMessage"] = "Problemas al subir la imagen. El problema ha sido notificado al equipo de desarrollo, por favor, intentalo nuevamente más tarde";
             } else {
-                $return["errorMessage"] = $e->getMessage();
+                $return["errorMessage"] = $e->getMessage() . " " . $error;
             }
             error_log("[".date("Y-m-d H:i:s")."] [reserves/uploadLicenseWarning] ERROR: ".$e->getMessage());
             if ($request->getHost() == "www.arriendas.cl" && $e->getCode() < 2) {
