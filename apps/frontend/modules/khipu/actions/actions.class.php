c2ea961c638f6511f5f32ed64b5f328751236f3c
sergioquez <squezada2b08@gmail.com>
2015-04-18 23:50:32 -0300

    fix advertencia foto licencia

--- apps/frontend/modules/khipu/actions/actions.class.php
+++ apps/frontend/modules/khipu/actions/actions.class.php
@@ -253,23 +253,40 @@ class khipuActions extends sfActions {
                         error_log("[khipu/notifyPayment] Enviando email al propietario");
                         $this->getMailer()->send($message);
 
-                        // Correo arrendatario
-                        $subject = "La reserva ha sido pagada";
-                        $body    = $this->getPartial('emails/paymentDoneRenter', array('Reserve' => $Reserve));
-                        $from    = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
-                        $to      = array($Renter->email => $Renter->firstname." ".$Renter->lastname);
+                        if($Renter->getDriverLicenseFile()){ // envía un mail al usuario dependiendo si tiene foto de licencia o no
+                            // Correo arrendatario
+                            $subject = "La reserva ha sido pagada";
+                            $body    = $this->getPartial('emails/paymentDoneRenter', array('Reserve' => $Reserve));
+                            $from    = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
+                            $to      = array($Renter->email => $Renter->firstname." ".$Renter->lastname);
 
-                        $message = Swift_Message::newInstance()
-                            ->setSubject($subject)
-                            ->setBody($body, 'text/html')
-                            ->setFrom($from)
-                            ->setTo($to)
-                            ->attach(Swift_Attachment::newInstance($contrato, 'contrato.pdf', 'application/pdf'))
-                            ->attach(Swift_Attachment::newInstance($formulario, 'formulario.pdf', 'application/pdf'))
-                            ->attach(Swift_Attachment::newInstance($reporte, 'reporte.pdf', 'application/pdf'))
-                            ->attach(Swift_Attachment::newInstance($pagare, 'pagare.pdf', 'application/pdf'));
+                            $message = Swift_Message::newInstance()
+                                ->setSubject($subject)
+                                ->setBody($body, 'text/html')
+                                ->setFrom($from)
+                                ->setTo($to)
+                                ->attach(Swift_Attachment::newInstance($contrato, 'contrato.pdf', 'application/pdf'))
+                                ->attach(Swift_Attachment::newInstance($formulario, 'formulario.pdf', 'application/pdf'))
+                                ->attach(Swift_Attachment::newInstance($reporte, 'reporte.pdf', 'application/pdf'))
+                                ->attach(Swift_Attachment::newInstance($pagare, 'pagare.pdf', 'application/pdf'));
 
-                        error_log("[khipu/notifyPayment] Enviando email al arrendatario");
+                            error_log("[khipu/notifyPayment] Enviando email al arrendatario");
+                        } else {
+
+                            $subject = "La reserva ha sido pagada";
+                            $body    = $this->getPartial('emails/paymentDoneRenterWithoutDriverLicense', array('Renter' => $Renter));
+                            $from    = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
+                            $to      = array($Renter->email => $Renter->firstname." ".$Renter->lastname);
+
+                            $message = Swift_Message::newInstance()
+                                ->setSubject($subject)
+                                ->setBody($body, 'text/html')
+                                ->setFrom($from)
+                                ->setTo($to);
+
+                            error_log("[khipu/notifyPayment] Enviando email al arrendatario sin licencia");
+
+                        }   
                         $this->getMailer()->send($message);
 
                         // Correo soporte
