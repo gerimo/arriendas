a97efbc5766003475b9434a70edc56b94233487f
francoinostroza <franco.inostrozah@gmail.com>
2015-05-12 21:30:01 -0300

    nueva interfaz de notificaciones nblgr

--- apps/nblgr/modules/main/templates/_header.php
+++ apps/nblgr/modules/main/templates/_header.php
@@ -1,40 +1,134 @@
 <header>
-    <nav class="navbar navbar-default navbar-fixed-top" id="header-navbar" role="navigation">
+    <nav class="navbar navbar-inverse" role="navigation">
         <div class="container">
-
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#header">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"> 
+                <span class="sr-only">Toggle navigation</span>
+                <i class="icon-cog"></i>
                 </button>
-                <a class="navbar-brand" href="<?php echo url_for('homepage') ?>"><img alt="Arriendas.cl" src="/images/newDesign/logobackend.svg" height="44" width="123"></a>
+                <a class="navbar-brand" href="<?php echo url_for('homepage') ?>"><img alt="Arriendas.cl" src="/images/newDesign/logo.svg" height="44" width="123"></a>
             </div>
 
-            <div class="collapse navbar-collapse" id="header">
-                
-                <ul class="nav navbar-nav" data-section="Autos">
-                    <li class="dropdown" data-section="Notificaciones">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Notificaciones<span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo url_for('notification') ?>">Notificaciones</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                
-
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo $sf_user->getAttribute('fullname') ?><span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo url_for('logout') ?>">Salir</a></li>
+                    <<!-- li class="dropdown">          
+                        <a href="javscript:;" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="icon-cog"></i>
+                            Settings
+                            <b class="caret"></b>
+                        </a>
+                        
+                        <ul class="dropdown-menu">
+                            <li><a href="./account.html">Account Settings</a></li>
+                            <li><a href="javascript:;">Privacy Settings</a></li>
+                            <li class="divider"></li>
+                            <li><a href="javascript:;">Help</a></li>
                         </ul>
+                            
+                    </li> -->
+
+                    <li class="dropdown">        
+                        <a href="javscript:;" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="icon-user"></i> 
+                            <?php echo $sf_user->getAttribute('fullname') ?>
+                            <b class="caret"></b>
+                        </a>
+                        
+                        <ul class="dropdown-menu">
+                            <!-- <li><a href="javascript:;">My Profile</a></li>
+                            <li><a href="javascript:;">My Groups</a></li>
+                            <li class="divider"></li> -->
+                            <li><a href="<?php echo url_for('logout') ?>">Logout</a></li>
+                        </ul>
+                        
                     </li>
                 </ul>
+    
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control input-sm search-query" placeholder="Search">
+                    </div>
+                </form>
             </div>
         </div>
     </nav>
+        
+    <div class="subnavbar">
+        <div class="subnavbar-inner">
+            <div class="container">
+                
+                <a href="javascript:;" class="subnav-toggle" data-toggle="collapse" data-target=".subnav-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <i class="icon-reorder"></i>
+                  
+                </a>
+
+                <div class="collapse subnav-collapse">
+                    <ul class="mainnav">
+                    
+                        <!--  <li class="active">
+                            <a href="<?php echo url_for('homepage') ?>">
+                                <i class="icon-home"></i>
+                                <span>Home</span>
+                            </a>                        
+                        </li> -->
+                        
+                        <li class="dropdown">                   
+                            <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="icon-th"></i>
+                                <span>Notificaciones</span>
+                                <b class="caret"></b>
+                            </a>        
+                        
+                            <ul class="dropdown-menu">
+                                <li><a href="<?php echo url_for('notification') ?>">Dueño</a></li>
+                                <li><a href="<?php echo url_for('homepage') ?>">Arrendatario</a></li>
+                            </ul>               
+                        </li>
+                        
+                        <!-- <li class="dropdown">                   
+                            <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="icon-copy"></i>
+                                <span>Sample Pages</span>
+                                <b class="caret"></b>
+                            </a>        
+                        
+                            <ul class="dropdown-menu">
+                                <li><a href="./pricing.html">Pricing Plans</a></li>
+                                <li><a href="./faq.html">FAQ's</a></li>
+                                <li><a href="./gallery.html">Gallery</a></li>
+                                <li><a href="./reports.html">Reports</a></li>
+                                <li><a href="./account.html">User Account</a></li>
+                            </ul>               
+                        </li>
+                        
+                        <li class="dropdown">                   
+                            <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="icon-external-link"></i>
+                                <span>Extra Pages</span>
+                                <b class="caret"></b>
+                            </a>    
+                        
+                            <ul class="dropdown-menu">
+                                <li><a href="./login.html">Login</a></li>
+                                <li><a href="./signup.html">Signup</a></li>
+                                <li><a href="./error.html">Error</a></li>
+                                <li class="dropdown-submenu">
+                                    <a tabindex="-1" href="#">More options</a>
+                                    <ul class="dropdown-menu">
+                                      <li><a tabindex="-1" href="#">Second level</a></li>
+
+                                      <li><a href="#">Second level</a></li>
+                                      <li><a href="#">Second level</a></li>
+                                    </ul>
+                                </li>
+                            </ul>                   
+                        </li> -->
+                    </ul>
+                </div> 
+            </div>
+        </div>
+    </div>
 </header>
 
 
--- apps/nblgr/templates/layout.php
+++ apps/nblgr/templates/layout.php
@@ -8,65 +8,43 @@
         <?php include_metas() ?>
         <?php include_title() ?>
 
-        <a style="color:black" href="" rel="shortcut icon" type="image/x-icon">Arriendas</a>
-
-        <!-- JQuery UI -->
-        <link href="/css/newDesign/jquery-ui/jquery-ui.min.css" rel="stylesheet" type="text/css">
-        <link href="/css/newDesign/jquery-ui/jquery-ui.structure.min.css" rel="stylesheet" type="text/css">
-        <link href="/css/newDesign/jquery-ui/jquery-ui.theme.min.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="apple-mobile-web-app-capable" content="yes">    
         
-        <!-- Bootstrap -->
-        <link href="/css/newDesign/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="/css/newDesign/nblgr/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/newDesign/nblgr/bootstrap-responsive.min.css" rel="stylesheet">
+        
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600" rel="stylesheet">
+        <link href="/css/newDesign/nblgr/font-awesome.css" rel="stylesheet">        
+        
+        <link href="/css/newDesign/nblgr/ui-lightness/jquery-ui-1.10.0.custom.min.css" rel="stylesheet">
+        
+        <link href="/css/newDesign/nblgr/base-admin-3.css" rel="stylesheet">
+        <link href="/css/newDesign/nblgr/base-admin-3-responsive.css" rel="stylesheet">
+        
+        <link href="/css/newDesign/nblgr/pages/dashboard.css" rel="stylesheet">   
 
-        <!-- Font Awesome -->
-        <link href="/css/newDesign/font-awesome/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="/css/newDesign/nblgr/custom.css" rel="stylesheet">
 
-        <!-- Complementos -->
-        <link href="/css/newDesign/datetimepicker/jquery.datetimepicker.css" rel="stylesheet" type="text/css">
-        <link href="/css/newDesign/slick/slick.css" rel="stylesheet" type="text/css">
-        <link href="/css/newDesign/raty/jquery.raty.css" rel="stylesheet" type="text/css">
-        <link href="/css/newDesign/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
-        <link href="/css/newDesign/colorbox/colorbox.css" rel="stylesheet" type="text/css">
-        <!-- <link href="/css/newDesign/datatablesp/jquery.dataTables_themeroller.css" rel="stylesheet" type="text/css"> -->
-        <link href="/css/newDesign/datatables/dataTables.responsive.css" rel="stylesheet" type="text/css">
-        <link href="/css/newDesign/fileinput/fileinput.min.css" rel="stylesheet" type="text/css">
-
-        <!-- Summernote -->
-        <link href="/css/newDesign/summernote/summernote.css" rel="stylesheet" type="text/css">
-
-        <!-- Estilos para todo el proyecto -->
-        <link href="/css/newDesign/backend.css" rel="stylesheet" type="text/css">
-
-        <!-- Fonts -->
-        <link href="http://fonts.googleapis.com/css?family=Roboto:300,350,400,500,600,700,800" rel="stylesheet" type="text/css">
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,500,600,700,800,400italic,700italic" rel="stylesheet" type="text/css">
     </head>
 
     <body>
 
-        <!-- JQuery -->
-        <script src="/js/newDesign/jquery-2.1.3.min.js" type="text/javascript"></script>
+        <script src="/js/newDesign/nblgr/libs/jquery-1.9.1.min.js"></script>
+        <script src="/js/newDesign/nblgr/libs/jquery-ui-1.10.0.custom.min.js"></script>
+        <script src="/js/newDesign/nblgr/libs/bootstrap.min.js" type="text/javascript"></script>
 
-        <!-- JQuery UI -->
-        <script src="/js/newDesign/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
-        
-        <!-- Bootstrap -->
-        <script src="/js/newDesign/bootstrap.min.js" type="text/javascript"></script>
+        <script src="/js/newDesign/nblgr/plugins/flot/jquery.flot.js"></script>
+        <script src="/js/newDesign/nblgr/plugins/flot/jquery.flot.pie.js"></script>
+        <script src="/js/newDesign/nblgr/plugins/flot/jquery.flot.resize.js"></script>
 
-        <!-- Complementos -->
-        <script src="/js/newDesign/datetimepicker/jquery.datetimepicker.js" type="text/javascript"></script>
-        <script src="/js/newDesign/slick/slick.min.js" type="text/javascript"></script>
-        <script src="/js/newDesign/raty/jquery.raty.js" type="text/javascript"></script>
-        <script src="/js/newDesign/number/jquery.number.min.js" type="text/javascript"></script>
-        <script src="/js/newDesign/jquery-form/jquery.form.min.js" type="text/javascript"></script>
-        <script src="/js/newDesign/datatables/jquery.dataTables.min.js" type="text/javascript"></script>
-        <script src="/js/newDesign/datatables/dataTables.responsive.min.js" type="text/javascript"></script>
-        <script src="/js/newDesign/formToWizard/formToWizard.js" type="text/javascript"></script>
-        <script src="/js/newDesign/colorbox/jquery.colorbox-min.js" type="text/javascript"></script>
-        <script src="/js/newDesign/colorbox/jquery.colorbox.js" type="text/javascript"></script>
-        <script src="/js/newDesign/fileinput/fileinput.min.js" type="text/javascript"></script>
-        
-        <script src="/js/newDesign/summernote/summernote.js" type="text/javascript"></script>    
+        <script src="/js/newDesign/nblgr/Application.js"></script>
+
+        <script src="/js/newDesign/nblgr/charts/area.js"></script>
+        <script src="/js/newDesign/nblgr/charts/donut.js"></script>
+
+
+
 
         <?php include_component('main', 'header') ?>
 
