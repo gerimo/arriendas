148a4a11937bdb883380ae77ab11a4915e266da9
Cristóbal Medina Moenne <cristobal@arriendas.cl>
2015-10-09 13:53:36 -0300

    fix mobile webpay

--- apps/mobile/modules/webpay/actions/actions.class.php
+++ apps/mobile/modules/webpay/actions/actions.class.php
@@ -181,6 +181,160 @@ class webpayActions extends sfActions {
 
                     $this->voucherUrl = $transactionResultOutput->urlRedirection;
                     $this->token = $token;
+
+                    error_log("[webpay/processPayment] Nuevo pago recibido");
+                    
+                    $Renter = $Reserve->getUser();
+                    $Owner  = $Reserve->getCar()->getUser();
+
+                    $Functions = new Functions;
+                    $Functions->generarNroFactura($Reserve, $Transaction);
+
+                    $Reserve->setFechaPago(date("Y-m-d H:i:s"));
+                    $Reserve->save();
+
+                    $formulario = $Functions->generarFormulario(NULL, $Reserve->token);
+                    $reporte    = $Functions->generarReporte($Reserve->getCar()->id);
+                    $contrato   = $Functions->generarContrato($Reserve->token);
+                    $pagare     = $Functions->generarPagare($Reserve->token);
+
+                    // Correo dueño
+                    $subject = "¡Has recibido un pago! Apruébalo ahora";
+                    $body    = $this->getPartial('emails/paymentDoneOwner', array('Reserve' => $Reserve));
+                    $from    = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
+                    $to      = array($Owner->email => $Owner->firstname." ".$Owner->lastname);
+
+                    $message = Swift_Message::newInstance()
+                        ->setSubject($subject)
+                        ->setBody($body, 'text/html')
+                        ->setFrom($from)
+                        ->setTo($to)
+                        ->attach(Swift_Attachment::newInstance($contrato, 'contrato.pdf', 'application/pdf'))
+                        ->attach(Swift_Attachment::newInstance($formulario, 'formulario.pdf', 'application/pdf'))
+                        ->attach(Swift_Attachment::newInstance($reporte, 'reporte.pdf', 'application/pdf'));
+                    
+                    if (!is_null($Renter->getDriverLicenseFile())) {
+                        $filepath = $Renter->getDriverLicenseFile();
+                        if (is_file($filepath)) {
+                            $message->attach(Swift_Attachment::fromPath($Renter->getDriverLicenseFile())->setFilename("LicenciaArrendatario-".$Renter->getLicenceFileName()));
+                        }
+                    }
+                    
+                    error_log("[webpay/processPayment] Enviando email al propietario");
+                    $this->getMailer()->send($message);
+
+                    if($Renter->getDriverLicenseFile()){ // envía un mail al usuario dependiendo si tiene foto de licencia o no
+                        // Correo arrendatario
+                        $subject = "La reserva ha sido pagada";
+                        $body    = $this->getPartial('emails/paymentDoneRenter', array('Reserve' => $Reserve));
+                        $from    = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
+                        $to      = array($Renter->email => $Renter->firstname." ".$Renter->lastname);
+
+                        $message = Swift_Message::newInstance()
+                            ->setSubject($subject)
+                            ->setBody($body, 'text/html')
+                            ->setFrom($from)
+                            ->setTo($to)
+                            ->attach(Swift_Attachment::newInstance($contrato, 'contrato.pdf', 'application/pdf'))
+                            ->attach(Swift_Attachment::newInstance($formulario, 'formulario.pdf', 'application/pdf'))
+                            ->attach(Swift_Attachment::newInstance($reporte, 'reporte.pdf', 'application/pdf'))
+                            ->attach(Swift_Attachment::newInstance($pagare, 'pagare.pdf', 'application/pdf'));
+
+                        error_log("[webpay/processPayment] Enviando email al arrendatario");
+                    } else {
+
+                        $subject = "La reserva ha sido pagada";
+                        $body    = $this->getPartial('emails/paymentDoneRenterWithoutDriverLicense', array('Renter' => $Renter));
+                        $from    = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
+                        $to      = array($Renter->email => $Renter->firstname." ".$Renter->lastname);
+
+                        $message = Swift_Message::newInstance()
+                            ->setSubject($subject)
+                            ->setBody($body, 'text/html')
+                            ->setFrom($from)
+                            ->setTo($to);
+
+                        error_log("[webpay/processPayment] Enviando email al arrendatario sin licencia");
+
+                    }   
+                    $this->getMailer()->send($message);
+
+                    // Correo soporte
+                    $subject = "Nuevo pago. Reserva: ".$Reserve->id;
+                    $body    = $this->getPartial('emails/paymentDoneSupport', array('Reserve' => $Reserve));
+                    $from    = array("no-reply@arriendas.cl" => "Notificaciones Arriendas.cl");
+                    $to      = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
+
+                    $message = Swift_Message::newInstance()
+                        ->setSubject($subject)
+                        ->setBody($body, 'text/html')
+                        ->setFrom($from)
+                        ->setTo($to)
+                        ->setBcc(array("cristobal@arriendas.cl" => "Cristóbal Medina Moenne"))
+                        ->attach(Swift_Attachment::newInstance($contrato, 'contrato.pdf', 'application/pdf'))
+                        ->attach(Swift_Attachment::newInstance($formulario, 'formulario.pdf', 'application/pdf'))
+                        ->attach(Swift_Attachment::newInstance($reporte, 'reporte.pdf', 'application/pdf'));
+
+                    if (!is_null($Renter->getDriverLicenseFile())) {
+                        $filepath = $Renter->getDriverLicenseFile();
+                        if (is_file($filepath)) {
+                            $message->attach(Swift_Attachment::fromPath($Renter->getDriverLicenseFile())->setFilename("LicenciaArrendatario-".$Renter->getLicenceFileName()));
+                        }
+                    }
+                    
+                    error_log("[webpay/processPayment] Enviando email a soporte");
+                    $this->getMailer()->send($message);
+
+                    // Crea la fila calificaciones habilitada para la fecha de término de reserva + 2 horas (solo si no es una extension de otra reserva)
+                    if (!$Reserve->getIdPadre()) {
+
+                        $Rating = new Rating();
+                        $Rating->setFechaHabilitadaDesde($Reserve->getFechaHabilitacionRating());
+                        $Rating->setIdOwner($Owner->id);
+                        $Rating->setIdRenter($Renter->id);
+                        $Rating->save();
+
+                        // Actualiza rating_id en la tabla Reserve
+                        $ratingId = $Rating->id;
+                        $Reserve->setRatingId($ratingId);
+                        $Reserve->save();
+                    }
+
+                    // Almacena reserveId en la tabla mail calificaciones
+                    $Reserve->encolarMailCalificaciones();
+
+                    error_log("[webpay/processPayment] ---------- HABEMUS PAGO --------");
+
+                    $OpportunityQueue = Doctrine_Core::getTable('OpportunityQueue')->findOneByReserveId($Transaction->getReserveId());
+                    if (!$OpportunityQueue) {
+                        $OpportunityQueue = new OpportunityQueue;
+                        $OpportunityQueue->setReserveId($Transaction->getReserveId());
+                        $OpportunityQueue->setPaidAt($Reserve->getFechaPago());
+                        $OpportunityQueue->save();
+                    }
+
+                    if(!$Reserve->getUser()->getDriverLicenseFile()){
+
+                        $subject = "Pago de usuario sin licencia de conducir";
+                        $body    = $this->getPartial('emails/paymentDoneUnverifiedUser', array('Transaction' => $Transaction));
+                        $from    = array("no-reply@arriendas.cl" => "Notificaciones Arriendas.cl");
+                        $to      = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
+
+                        $message = Swift_Message::newInstance()
+                            ->setSubject($subject)
+                            ->setBody($body, 'text/html')
+                            ->setFrom($from)
+                            ->setTo($to)
+                            ->setBcc(array("cristobal@arriendas.cl" => "Cristóbal Medina Moenne"));
+                        
+                        $this->getMailer()->send($message);
+                    }
+
+                    // Notificaciones
+                    Notification::make($Renter->id, 3, $Reserve->id); // pago
+                    
+                    $Transaction->setCompleted(true);
+                    $Transaction->save();
                 } else {
                     $this->getRequest()->setParameter("reserveId", $Reserve->getId());
                     $this->forward("webpay", "processPaymentRejected");
@@ -560,160 +714,6 @@ class webpayActions extends sfActions {
                     $this->getRequest()->setParameter("reserveId", $Reserve->getId());
                     $this->forward("webpay", "processPaymentRejected");
                 }
-
-                error_log("[webpay/processPayment] Nuevo pago recibido");
-
-                $Renter = $Reserve->getUser();
-                $Owner  = $Reserve->getCar()->getUser();
-
-                $Functions = new Functions;
-                $Functions->generarNroFactura($Reserve, $Transaction);
-
-                $Reserve->setFechaPago(date("Y-m-d H:i:s"));
-                $Reserve->save();
-
-                $formulario = $Functions->generarFormulario(NULL, $Reserve->token);
-                $reporte    = $Functions->generarReporte($Reserve->getCar()->id);
-                $contrato   = $Functions->generarContrato($Reserve->token);
-                $pagare     = $Functions->generarPagare($Reserve->token);
-
-                // Correo dueño
-                $subject = "¡Has recibido un pago! Apruébalo ahora";
-                $body    = $this->getPartial('emails/paymentDoneOwner', array('Reserve' => $Reserve));
-                $from    = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
-                $to      = array($Owner->email => $Owner->firstname." ".$Owner->lastname);
-
-                $message = Swift_Message::newInstance()
-                    ->setSubject($subject)
-                    ->setBody($body, 'text/html')
-                    ->setFrom($from)
-                    ->setTo($to)
-                    ->attach(Swift_Attachment::newInstance($contrato, 'contrato.pdf', 'application/pdf'))
-                    ->attach(Swift_Attachment::newInstance($formulario, 'formulario.pdf', 'application/pdf'))
-                    ->attach(Swift_Attachment::newInstance($reporte, 'reporte.pdf', 'application/pdf'));
-                
-                if (!is_null($Renter->getDriverLicenseFile())) {
-                    $filepath = $Renter->getDriverLicenseFile();
-                    if (is_file($filepath)) {
-                        $message->attach(Swift_Attachment::fromPath($Renter->getDriverLicenseFile())->setFilename("LicenciaArrendatario-".$Renter->getLicenceFileName()));
-                    }
-                }
-                
-                error_log("[webpay/processPayment] Enviando email al propietario");
-                $this->getMailer()->send($message);
-
-                if($Renter->getDriverLicenseFile()){ // envía un mail al usuario dependiendo si tiene foto de licencia o no
-                    // Correo arrendatario
-                    $subject = "La reserva ha sido pagada";
-                    $body    = $this->getPartial('emails/paymentDoneRenter', array('Reserve' => $Reserve));
-                    $from    = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
-                    $to      = array($Renter->email => $Renter->firstname." ".$Renter->lastname);
-
-                    $message = Swift_Message::newInstance()
-                        ->setSubject($subject)
-                        ->setBody($body, 'text/html')
-                        ->setFrom($from)
-                        ->setTo($to)
-                        ->attach(Swift_Attachment::newInstance($contrato, 'contrato.pdf', 'application/pdf'))
-                        ->attach(Swift_Attachment::newInstance($formulario, 'formulario.pdf', 'application/pdf'))
-                        ->attach(Swift_Attachment::newInstance($reporte, 'reporte.pdf', 'application/pdf'))
-                        ->attach(Swift_Attachment::newInstance($pagare, 'pagare.pdf', 'application/pdf'));
-
-                    error_log("[webpay/processPayment] Enviando email al arrendatario");
-                } else {
-
-                    $subject = "La reserva ha sido pagada";
-                    $body    = $this->getPartial('emails/paymentDoneRenterWithoutDriverLicense', array('Renter' => $Renter));
-                    $from    = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
-                    $to      = array($Renter->email => $Renter->firstname." ".$Renter->lastname);
-
-                    $message = Swift_Message::newInstance()
-                        ->setSubject($subject)
-                        ->setBody($body, 'text/html')
-                        ->setFrom($from)
-                        ->setTo($to);
-
-                    error_log("[webpay/processPayment] Enviando email al arrendatario sin licencia");
-
-                }   
-                $this->getMailer()->send($message);
-
-                // Correo soporte
-                $subject = "Nuevo pago. Reserva: ".$Reserve->id;
-                $body    = $this->getPartial('emails/paymentDoneSupport', array('Reserve' => $Reserve));
-                $from    = array("no-reply@arriendas.cl" => "Notificaciones Arriendas.cl");
-                $to      = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
-
-                $message = Swift_Message::newInstance()
-                    ->setSubject($subject)
-                    ->setBody($body, 'text/html')
-                    ->setFrom($from)
-                    ->setTo($to)
-                    ->setBcc(array("cristobal@arriendas.cl" => "Cristóbal Medina Moenne"))
-                    ->attach(Swift_Attachment::newInstance($contrato, 'contrato.pdf', 'application/pdf'))
-                    ->attach(Swift_Attachment::newInstance($formulario, 'formulario.pdf', 'application/pdf'))
-                    ->attach(Swift_Attachment::newInstance($reporte, 'reporte.pdf', 'application/pdf'));
-
-                if (!is_null($Renter->getDriverLicenseFile())) {
-                    $filepath = $Renter->getDriverLicenseFile();
-                    if (is_file($filepath)) {
-                        $message->attach(Swift_Attachment::fromPath($Renter->getDriverLicenseFile())->setFilename("LicenciaArrendatario-".$Renter->getLicenceFileName()));
-                    }
-                }
-                
-                error_log("[webpay/processPayment] Enviando email a soporte");
-                $this->getMailer()->send($message);
-
-                // Crea la fila calificaciones habilitada para la fecha de término de reserva + 2 horas (solo si no es una extension de otra reserva)
-                if (!$Reserve->getIdPadre()) {
-
-                    $Rating = new Rating();
-                    $Rating->setFechaHabilitadaDesde($Reserve->getFechaHabilitacionRating());
-                    $Rating->setIdOwner($Owner->id);
-                    $Rating->setIdRenter($Renter->id);
-                    $Rating->save();
-
-                    // Actualiza rating_id en la tabla Reserve
-                    $ratingId = $Rating->id;
-                    $Reserve->setRatingId($ratingId);
-                    $Reserve->save();
-                }
-
-                // Almacena reserveId en la tabla mail calificaciones
-                $Reserve->encolarMailCalificaciones();
-
-                error_log("[webpay/processPayment] ---------- HABEMUS PAGO --------");
-
-                $OpportunityQueue = Doctrine_Core::getTable('OpportunityQueue')->findOneByReserveId($Transaction->getReserveId());
-                if (!$OpportunityQueue) {
-                    $OpportunityQueue = new OpportunityQueue;
-                    $OpportunityQueue->setReserveId($Transaction->getReserveId());
-                    $OpportunityQueue->setPaidAt($Reserve->getFechaPago());
-                    $OpportunityQueue->save();
-                }
-
-                if(!$Reserve->getUser()->getDriverLicenseFile()){
-
-                    $subject = "Pago de usuario sin licencia de conducir";
-                    $body    = $this->getPartial('emails/paymentDoneUnverifiedUser', array('Transaction' => $Transaction));
-                    $from    = array("no-reply@arriendas.cl" => "Notificaciones Arriendas.cl");
-                    $to      = array("soporte@arriendas.cl" => "Soporte Arriendas.cl");
-
-                    $message = Swift_Message::newInstance()
-                        ->setSubject($subject)
-                        ->setBody($body, 'text/html')
-                        ->setFrom($from)
-                        ->setTo($to)
-                        ->setBcc(array("cristobal@arriendas.cl" => "Cristóbal Medina Moenne"));
-                    
-                    $this->getMailer()->send($message);
-                }
-
-                // Notificaciones
-                Notification::make($Renter->id, 3, $Reserve->id); // pago
-                
-                $Transaction->setCompleted(true);
-                $Transaction->save();
 
                 $this->redirect('reserves');
             } else {
