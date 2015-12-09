ccc4a6e0f1d02c0ee49b2f94dce7ba44647ed16c
Cristóbal Medina Moenne <cristobal@arriendas.cl>
2015-12-09 10:16:46 -0300

    cambios en el correo de pago para el arrendatario

--- apps/mobile/modules/emails/templates/_paymentDoneRenter.php
+++ apps/mobile/modules/emails/templates/_paymentDoneRenter.php
@@ -6,9 +6,9 @@
 
 <p><?php echo $Renter->firstname ?>, has pagado la reserva.</p>
 
-<p>El dueño tiene 4 horas para aprobar la reserva. Si no lo hace, el sistema te ofrecerá opciones de reemplazo.</p> 
+<p>Ahora debes ponerte en contacto con el dueño del auto para coordinar la entrega, imprimir los contratos de arriendo (contrato, formulario y pagare) y presentarlos el día del retiro del auto.</p>
 
-<p>En caso de que el precio del auto de reemplazo sea mayor, arriendas pondrá un 15% del valor del auto ya pagado.</p> 
+<p>En caso de que el auto pagado no este disponible por favor escríbenos a <a href="mailto:soporte@arriendas.cl">soporte@arriendas.cl</a>.</p>
 
 <h3>Datos del propietario</h3>
 
